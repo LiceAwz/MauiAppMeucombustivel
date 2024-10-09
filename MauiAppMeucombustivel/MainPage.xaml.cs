e9db3a2f5aeefd704c48ec5a308f8eb6d849ea8c
LiceAwz <liceawazu@gmail.com>
2024-10-09 18:53:12 -0300

    Adição de campos para colocar marca e modelo do veículo

--- MauiAppMeucombustivel/MainPage.xaml.cs
+++ MauiAppMeucombustivel/MainPage.xaml.cs
@@ -11,24 +11,30 @@
         {
             try
             {
-
-                double etanal = Convert.ToDouble(txt_etanol.Text);
+                // Obtém os valores inseridos para etanol e gasolina
+                double etanol = Convert.ToDouble(txt_etanol.Text);
                 double gasolina = Convert.ToDouble(txt_gasolina.Text);
 
+                // Obtém os valores da marca e modelo do veículo
+                string marca = txt_marca_do_veiculo.Text;
+                string modelo = txt_modelo_do_veiculo.Text;
+
                 string msg = "";
-                if (etanal <= (gasolina * 0.7))
+                if (etanol <= (gasolina * 0.7))
                 {
-                    msg = "O etanol está compensando.";
+                    msg = $"O etanol está compensando para o seu {marca} {modelo}.";
                 }
                 else
                 {
-                    msg = "A gasolina está compensado.";
+                    msg = $"A gasolina está compensando para o seu {marca} {modelo}.";
                 }
 
+                // Exibe a mensagem
                 DisplayAlert("Calculado", msg, "OK");
 
             } catch (Exception ex)
             {
+                // Exibe a mensagem de erro caso ocorra algum problema
                 DisplayAlert("Ops", ex.Message, "Fechar");
             }
 
